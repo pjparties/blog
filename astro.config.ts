49ac27323a0aa06ceec4d169de127367284e121f
parth <parthhjuneja@gmail.com>
2024-06-21 06:00:39 +0530

    add: vercel web analytics

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,7 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -39,4 +40,8 @@ export default defineConfig({
     },
   },
   scopedStyleStrategy: "where",
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
